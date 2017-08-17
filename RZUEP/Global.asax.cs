4ab3c5a441ae5d0d5e9dfde19c8e4a9ac92ae6d6
webre <webre@DESKTOP-NVDLLVJ>
2017-08-17 15:31:55 +0200

    Zmiana2.20170817

--- RZUEP/Global.asax.cs
+++ RZUEP/Global.asax.cs
@@ -16,7 +16,7 @@ namespace RZUEP
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-            GlobalVariables.Aktualizacja = false; 
+            GlobalVariables.Aktualizacja = false;
         }
     }
 }
