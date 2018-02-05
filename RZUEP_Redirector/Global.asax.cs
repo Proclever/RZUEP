3fd8eef25d1e55d6d7a61b93b866ac4ceca6feba
webre <webre@DESKTOP-NVDLLVJ>
2018-02-05 23:00:38 +0100

    redirector

---
+++ RZUEP_Redirector/Global.asax.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace RZUEP_Redirector
+{
+    public class Global : System.Web.HttpApplication
+    {
+        protected void Application_BeginRequest()
+        {
+            UriBuilder uri = new UriBuilder(Context.Request.Url);
+            uri.Scheme = "http";
+            Response.Redirect(uri.ToString().Replace("rzuep.apphb.com", "rzuep.pl"));
+        }
+    }
+}
